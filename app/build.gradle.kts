f071bec14d3ec2d1c64dc791f79f7c406c7c1b0f
Valladaresst <valladares.stanley123@gmail.com>
2025-09-17 23:54:40 -0600

    Ultimo

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,7 +27,7 @@ android {
         }
     }
 
-    // Java/Kotlin 17
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
@@ -36,31 +36,31 @@ android {
 }
 
 dependencies {
-    // AndroidX (del catalog fijado)
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
-    implementation(libs.androidx.activity.ktx)   // 👈 ojo: .activity.ktx
+    implementation(libs.androidx.activity.ktx)
     implementation(libs.androidx.constraintlayout)
 
-    // Firebase: BoM + ktx (no mezclar con firebase-auth de catalog)
+
     implementation(platform("com.google.firebase:firebase-bom:33.3.0"))
     implementation("com.google.firebase:firebase-auth-ktx")
     dependencies {
-        // Firebase BOM (si no lo tienes)
+
         implementation(platform("com.google.firebase:firebase-bom:33.4.0"))
 
-        // Realtime Database (NECESARIA)
+
         implementation("com.google.firebase:firebase-database-ktx")
     }
 
 
-    // Test
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.espresso.core)
 
-    // (Opcional) Bloqueo defensivo por si otra lib intenta subir versiones
+
     constraints {
         implementation("androidx.core:core:1.13.1")
         implementation("androidx.core:core-ktx:1.13.1")
